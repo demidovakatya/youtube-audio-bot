586e8fea6fdeaf7042c9d509da6f55291d64d606
Katya <demidovakatya@gmail.com>
2018-01-09 21:36:19 +0300

    small fixes

--- base.py
+++ base.py
@@ -1,23 +1,29 @@
+import logging
+
 from os import getenv
+from telegram.ext import CommandHandler
+from telegram.ext import Updater
+from telegram.ext import MessageHandler
+
+
 TOKEN = getenv("TELEGRAM_YOUTUBE_AUDIO_BOT_TOKEN")
 
-from telegram.ext import Updater
-updater = Updater(token=TOKEN)
-
-dispatcher = updater.dispatcher
-
-import logging
-logging.basicConfig(format='[ %(asctime) ] %(message)',
-                    level=logging.INFO)
+logging.basicConfig(format='[ %(asctime) ] %(message)', level=logging.INFO)
 
 
+# COMMAND HANDLERS
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=update.message.text)
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-from telegram.ext import CommandHandler
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    updater = Updater(token=TOKEN)
 
-updater.start_polling()
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
