459a8a33ed4e20799445a9803847535f0700278b
Katya <demidovakatya@gmail.com>
2018-01-10 21:35:25 +0300

    small fixes

--- base.py
+++ base.py
@@ -3,24 +3,40 @@ import logging
 from os import getenv
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
+from telegram.ext import Filters
 from telegram.ext import MessageHandler
 
 
 TOKEN = getenv("TELEGRAM_YOUTUBE_AUDIO_BOT_TOKEN")
 
-logging.basicConfig(format='[ %(asctime) ] %(message)', level=logging.INFO)
+logging.basicConfig(format='[ %(asctime)s ] %(message)s', level=logging.INFO)
 
 
 # COMMAND HANDLERS
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! Send a youtube link!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Send a youtube link.')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
 
 def main():
     updater = Updater(token=TOKEN)
 
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help))
+
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_polling()
 
