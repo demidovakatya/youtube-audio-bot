47e03a4c67e06d7c18fcc9d0ac5a22f3c420eccc
Katya <demidovakatya@gmail.com>
2018-01-15 21:22:07 +0300

    upd

--- base.py
+++ base.py
@@ -4,7 +4,8 @@ from os import getenv
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
 from telegram.ext import Filters
-from telegram.ext import MessageHandler
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import InlineQueryHandler
 from telegram import InlineQueryResult
 
 
@@ -33,6 +34,23 @@ def echo(bot, update):
                      text=update.message.text)
 
 
+def inline_processing(bot, update):
+    query = update.inline_query.query
+    if not query:
+        return
+    result = update.message.text # for now
+    bot.answer_inline_query(update.inline_query.id, result)
+
+
+def unknown(bot, update):
+    """If someone tries to send unknown commands to the bot, this MessageHandler
+    with a command filter will reply to all commands that were
+    not recognized by the previous handlers."""
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="I have no idea what you are talking about.")
+
+
+
 def main():
     updater = Updater(token=TG_TOKEN)
 
@@ -44,6 +62,10 @@ def main():
 
     # Initialize Message handlers
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.commands, unknown))
+
+    # Initialize Inline handlers
+    dispatcher.add_handler(InlineQueryHandler(inline_processing))
 
     # Start the Bot
     updater.start_polling()
